99588205cc9d68188df6374d724e00a0b8395b48
Michael Wong <mik9080@gmail.com>
2017-09-28 21:46:21 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,20 @@
 //define functions here
-
+function getIt(){
+  $('p').on('click',alert("Hey!"))
+}
+function frameIt(){
+  $('img').on('load',this.addClass('.tasty'))
+}
+function pressIt(){
+  $('input').on('keydown',function(e){
+    if (e.which==71) {
+      alert('G was pressed')
+    }
+  })
+}
+function submitIt(){
+  $('form').on('submit',alert("Your form is going to be submitted now."))
+}
 $(document).ready(function(){
 
 // call functions here
